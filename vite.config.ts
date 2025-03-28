8fcd979e6c32a2f1518febd838ef602438e2bf47
siva0704 <vaibhavabg1234@gmail.com>
2025-03-29 00:36:55 +0530

    renamed the repo name in json and config

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: '/Cascade/',
+  base: '/Cascade-Space/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
